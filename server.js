91cb1f79a95738265279d9448467ed43b7333294
kira libe <realweirdo904@gmail.com>
2021-06-16 10:19:02 +0300

    setting up connection with mongodb and transferring routing logic to app.js

--- server.js
+++ server.js
@@ -1,15 +1,14 @@
-const http = require('http');
 const dotenv = require('dotenv');
+
+const app = require('./app');
+const connectDB = require('./config/db');
 
 dotenv.config({ path: './config/config.env' });
 
 const PORT = process.env.PORT || 5000;
-const server = http.createServer((req, res) => {
-	res.end('<h1>Hello world</h1>');
-});
 
-server.listen(PORT, () => {
+connectDB();
+app.listen(PORT, (err) => {
+	if (err) return console.error('something went wrong');
 	console.log(`server running on port ${PORT}`);
 });
-
-console.log(process.env.PORT);
