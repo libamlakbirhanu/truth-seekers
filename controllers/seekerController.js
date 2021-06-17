90a795881d1ed9b4b59ca143f6dc470866e52314
kira libe <realweirdo904@gmail.com>
2021-06-17 12:40:38 +0300

    using the user info from the request header

--- controllers/seekerController.js
+++ controllers/seekerController.js
@@ -2,7 +2,7 @@ const Seeker = require('./../models/Seeker');
 
 exports.getSeeker = async (req, res, next) => {
 	try {
-		const doc = await Seeker.findById(req.params.id);
+		const doc = req.user;
 		res.status(200).json({
 			status: 'success',
 			result: doc,
