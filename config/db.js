b95001b900c7704ef08b48b0d8f4913a3d6eef0a
Libamlak Birhanu <realweirdo904@gmail.com>
2021-07-22 16:53:15 +0300

    adding remote mongo uri to the config file

--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 module.exports = connectDB = async () => {
 	try {
 		await mongoose.connect(
-			process.env.mongoURI,
+			process.env.mongoURI_remote,
 			{
 				useCreateIndex: true,
 				useUnifiedTopology: true,
